e4199c8e9987bfd2a8a8f3e8c13ad25e0c4fc38f
ChristopherRush <chris.uclan@gmail.com>
2018-03-28 15:23:01 +0200

    jhghj

---
+++ debug.py
@@ -0,0 +1,12 @@
+from papirus import PapirusComposite
+
+# Calling PapirusComposite this way will mean nothing is written to the screen until WriteAll is called
+textNImg = PapirusComposite(False [, rotation = rot])
+
+# Write text to the screen at selected point, with an Id
+# Nothing will show on the screen
+textNImg.AddText("hello world", 10, 10, Id="Start" )
+
+
+# Now display all elements on the screen
+textNImg.WriteAll()
