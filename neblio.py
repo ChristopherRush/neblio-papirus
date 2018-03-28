62dfb2d215824841b8331bc3b591fa72a2983fb1
ChristopherRush <chris.uclan@gmail.com>
2018-03-28 15:58:20 +0200

    balance info

--- neblio.py
+++ neblio.py
@@ -7,13 +7,16 @@ text = PapirusText()
 textNImg = PapirusComposite()
 # rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://nebliorpc:Dtmqe2aj1Fc35nKMKMrwyCKEYxnatVGpW9tvXhuXdTHt@127.0.0.1:8332")
-getinfo = rpc_connection.getinfo()["connections"]
 
+get_connection = rpc_connection.getinfo()["connections"]
+get_balance = rpc_connection.getinfo()["balance"]
 
-connections = ('Connections: %d' % getinfo)
+connections = ('Connections: %d' % get_connection)
+balance = ('Balance: %d' % get_balance)
+
 textNImg.AddText((connections), 10, 10, Id="Start")
 
-textNImg.AddText("hello world", 10, 30, Id="line2" )
+textNImg.AddText((balance), 10, 30, Id="line2" )
 
 
 
