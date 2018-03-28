f1f586afe9373f8b3f9c16e5388f8cd9256b5da7
ChristopherRush <chris.uclan@gmail.com>
2018-03-28 16:53:58 +0200

    dfgdfg

--- debug.py
+++ debug.py
@@ -32,53 +32,15 @@ while True:
         sys.exit()
 
     if GPIO.input(SW1) == False:
-        write_text(papirus, "One", SIZE)
+        print "1"
 
     if GPIO.input(SW2) == False:
-        write_text(papirus, "Two", SIZE)
+        print "2"
 
     if GPIO.input(SW3) == False:
-        write_text(papirus, "Three", SIZE)
+        print "3"
 
     if GPIO.input(SW4) == False:
-        write_text(papirus, "Four", SIZE)
-
-    if (SW5 != -1) and (GPIO.input(SW5) == False):
-        write_text(papirus, "Five", SIZE)
+        print "4"
 
     sleep(0.1)
-
-def write_text(papirus, text, size):
-
-    # initially set all white background
-    image = Image.new('1', papirus.size, WHITE)
-
-    # prepare for drawing
-    draw = ImageDraw.Draw(image)
-
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', size)
-
-    # Calculate the max number of char to fit on line
-    line_size = (papirus.width / (size*0.65))
-
-    current_line = 0
-    text_lines = [""]
-
-    # Compute each line
-    for word in text.split():
-        # If there is space on line add the word to it
-        if (len(text_lines[current_line]) + len(word)) < line_size:
-            text_lines[current_line] += " " + word
-        else:
-            # No space left on line so move to next one
-            text_lines.append("")
-            current_line += 1
-            text_lines[current_line] += " " + word
-
-    current_line = 0
-    for l in text_lines:
-        current_line += 1
-        draw.text( (0, ((size*current_line)-size)) , l, font=font, fill=BLACK)
-
-    papirus.display(image)
-    papirus.partial_update()
