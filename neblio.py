eb1b23d3a2cb7f845f7340154762cf03db1e436f
ChristopherRush <chris.uclan@gmail.com>
2018-03-28 15:28:26 +0200

    sdfsdf

--- neblio.py
+++ neblio.py
@@ -4,7 +4,7 @@ from papirus import PapirusText
 from papirus import PapirusComposite
 
 text = PapirusText()
-
+textNImg = PapirusComposite()
 # rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://nebliorpc:Dtmqe2aj1Fc35nKMKMrwyCKEYxnatVGpW9tvXhuXdTHt@127.0.0.1:8332")
 getinfo = rpc_connection.getinfo()["connections"]
