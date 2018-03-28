8df6bf8c8dbfa40ab14f55aa5c41ea37af336cde
ChristopherRush <chris.uclan@gmail.com>
2018-03-28 14:47:32 +0200

    removed

--- neblio.py
+++ neblio.py
@@ -2,9 +2,7 @@ from bitcoinrpc.authproxy import AuthServiceProxy
 from papirus import Papirus
 from papirus import PapirusText
 
-
-rot = 0, 90, 180 or 270
-text = PapirusText([rotation = rot])
+text = PapirusText()
 
 # rpc_user and rpc_password are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://nebliorpc:Dtmqe2aj1Fc35nKMKMrwyCKEYxnatVGpW9tvXhuXdTHt@127.0.0.1:8332")
