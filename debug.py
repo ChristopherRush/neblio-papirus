2cf6f208d9baa696c12f54998bd4f4fbda9d4fab
ChristopherRush <chris.uclan@gmail.com>
2018-03-29 18:07:47 +0200

    fhfgh

--- debug.py
+++ debug.py
@@ -4,7 +4,7 @@ import sys
 import ConfigParser
 config = ConfigParser.ConfigParser()
 config.read('/.neblio/neblio.conf')
-print config.get('SET','rpcuser')
+print config.get('set','rpcuser')
 
 from papirus import Papirus
 from time import sleep
