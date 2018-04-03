a5951473a3577e1a98909c3e8a3445fcec051e88
ChristopherRush <chris.uclan@gmail.com>
2018-04-03 17:08:56 +0200

    sdfsdf

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ with open(nebliopath, 'r') as f:
     b = f.read()
     config_string = a + b
 config = ConfigParser.ConfigParser()
-print config.read(config_string)
+print config.readfp(config_string)
 
 
 #test = config.get('dummy_section', 'rpcuser')
