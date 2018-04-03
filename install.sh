e42d43d6cea62e6da0ec1b7acb59972442726464
ChristopherRush <chris.uclan@gmail.com>
2018-04-03 13:33:57 +0200

    append config settings

--- install.sh
+++ install.sh
@@ -8,9 +8,16 @@ pip install python-bitcoinrpc
 
 git clone https://github.com/ChristopherRush/stakebox-papirus.git
 
-file=/home/pi/.neblio/neblio.conf
-if [ -e "$file" ]; then
-    echo "File exists"
+neblio=/home/pi/.neblio/neblio.conf
+if [ -e "$neblio" ]; then
+    touch $neblio
+    "neblio.conf file created"
+    echo "[config]" >> $neblio
+    echo "rpcpassword=neblio" >> $neblio
+    echo "rpcuser=nebliorpc" >> $neblio
+    echo "rpcport=8332" >> $neblio
+    echo "rpcallowip=127.0.0.1" >> $neblio
+    echo "configuration settigns appended"
 else
     echo "File does not exist"
 fi
