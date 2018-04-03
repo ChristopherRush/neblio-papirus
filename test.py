2036504919a50bca93bca1e5508575c9dd5a5801
ChristopherRush <chris.uclan@gmail.com>
2018-04-03 15:51:36 +0200

    cgfgd

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ nebliopath='/home/pi/.neblio/neblio.conf'
 
 
 with open(nebliopath, 'r') as f:
-    config_string = '[dummy_section]\n' + f.read()
+    config_string = '[dummy_section]\n' + f.read(rpcuser)
 config = configparser.ConfigParser()
-print config.get(rpcpassword)
+print config.read_string(config_string)
