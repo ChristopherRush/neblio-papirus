936e071ca25c635f02a51135292262e29f9d4ba3
ChristopherRush <chris.uclan@gmail.com>
2018-04-03 13:42:30 +0200

    check if application is installed

--- install.sh
+++ install.sh
@@ -8,16 +8,25 @@ pip install python-bitcoinrpc
 
 git clone https://github.com/ChristopherRush/stakebox-papirus.git
 
-neblio=/home/pi/.neblio/neblio.conf
-if [ -e "$neblio" ]; then
-    touch $neblio
-    "neblio.conf file created"
-    echo "[config]" >> $neblio
-    echo "rpcpassword=neblio" >> $neblio
-    echo "rpcuser=nebliorpc" >> $neblio
-    echo "rpcport=8332" >> $neblio
-    echo "rpcallowip=127.0.0.1" >> $neblio
-    echo "configuration settigns appended"
+neblioqt=/home/pi/.neblio/neblio-qt
+
+if [ -e "$neblioqt" ]; then
+    echo "neblio installed....checking config file"
+    neblio=/home/pi/.neblio/neblio.conf
+    if [ -e "$neblio" ]; then
+        echo "Config file already exists at $neblio"
+    else
+        echo "File does not exist"
+        touch $neblio
+        echo "neblio.conf file created"
+        echo "[config]" >> $neblio
+        echo "rpcpassword=neblio" >> $neblio
+        echo "rpcuser=nebliorpc" >> $neblio
+        echo "rpcport=8332" >> $neblio
+        echo "rpcallowip=127.0.0.1" >> $neblio
+        echo "configuration settigns appended"
+    fi
 else
-    echo "File does not exist"
+    echo "neblio not installed on this StakeBox"
+
 fi
--- test.sh
+++ test.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
-neblio=/home/pi/.neblio/neblio.conf
-if [ -e "$neblio" ]; then
-    echo "Config file already exists at $neblio"
+neblioqt=/home/pi/.neblio/neblio-qt
+
+if [ -e "$neblioqt" ]; then
+    echo "neblio installed....checking config file"
+    neblio=/home/pi/.neblio/neblio.conf
+    if [ -e "$neblio" ]; then
+        echo "Config file already exists at $neblio"
+    else
+        echo "File does not exist"
+        touch $neblio
+        echo "neblio.conf file created"
+        echo "[config]" >> $neblio
+        echo "rpcpassword=neblio" >> $neblio
+        echo "rpcuser=nebliorpc" >> $neblio
+        echo "rpcport=8332" >> $neblio
+        echo "rpcallowip=127.0.0.1" >> $neblio
+        echo "configuration settigns appended"
+    fi
 else
-    echo "File does not exist"
-    touch $neblio
-    echo "neblio.conf file created"
-    echo "[config]" >> $neblio
-    echo "rpcpassword=neblio" >> $neblio
-    echo "rpcuser=nebliorpc" >> $neblio
-    echo "rpcport=8332" >> $neblio
-    echo "rpcallowip=127.0.0.1" >> $neblio
-    echo "configuration settigns appended"
+    echo "neblio not installed on this StakeBox"
+
 fi
