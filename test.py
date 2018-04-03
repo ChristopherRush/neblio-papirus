b0ba16e24236771de72ff4ede1e4609c2c8ee03b
ChristopherRush <chris.uclan@gmail.com>
2018-04-03 16:25:54 +0200

    vbnvb

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ nebliopath='/home/pi/.neblio/neblio.conf'
 with open(nebliopath, 'r') as f:
     config_string = '[dummy_section]\n' + f.read()
 config = ConfigParser.ConfigParser()
-config.read_string(config_string)
+config.read(config_string)
 
 print config.get('dummy_section', 'rpcuser')
