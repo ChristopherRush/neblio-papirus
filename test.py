b4ac21d2319f263c4c633f5460df0937feefb05a
ChristopherRush <chris.uclan@gmail.com>
2018-04-03 16:55:49 +0200

    sdfsdf

--- test.py
+++ test.py
@@ -5,11 +5,13 @@ nebliopath='/home/pi/.neblio/neblio.conf'
 
 
 with open(nebliopath, 'r') as f:
-    print f.read()
-    config_string = '[dummy_section]\n' + f.read()
+    a = '[dummy_section]\n'
+    b = f.read()
+    config_string = a + b
 config = ConfigParser.ConfigParser()
 config.read(config_string)
 print config_string
+
 test = config.get('dummy_section', 'rpcuser')
 
 print test
