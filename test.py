535509e7a81c8761cecb3d537a2b629db6228ace
ChristopherRush <chris.uclan@gmail.com>
2018-04-09 13:05:32 +0100

    2w2w

--- test.py
+++ test.py
@@ -1,28 +1,85 @@
-from papirus import PapirusTextPos
-from time import sleep
+from __future__ import print_function
+
+import os
+import sys
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from datetime import datetime
+import time
 from papirus import Papirus
-while True:
-# Same as calling "PapirusTextPos(True [,rotation = rot])"
-    text = PapirusTextPos(False)
 
-# Write text to the screen at selected point, with an Id
-# "hello world" will appear on the screen at (10, 10), font size 20, straight away
-    text.AddText("hello world", 10, 10, Id="Start" )
+# Check EPD_SIZE is defined
+EPD_SIZE=0.0
+if os.path.exists('/etc/default/epd-fuse'):
+    exec(open('/etc/default/epd-fuse').read())
+if EPD_SIZE == 0.0:
+    print("Please select your screen size by running 'papirus-config'.")
+    sys.exit()
 
-# Add another line of text, at the default location
-# "Another line" will appear on screen at (0, 0), font size 20, straight away
-    text.AddText("Another line", Id="Top")
+# Running as root only needed for older Raspbians without /dev/gpiomem
+if not (os.path.exists('/dev/gpiomem') and os.access('/dev/gpiomem', os.R_OK | os.W_OK)):
+    user = os.getuid()
+    if user != 0:
+        print("Please run script as root")
+        sys.exit()
 
-    text.update()
-# Update the first line
-# "hello world" will disappear and "New Text" will be displayed straight away
-    text.UpdateText("Start", "hello world 1")
-    text.partial_update()
-# Remove The second line of text
-# "Another line" will be removed from the screen straight away
+WHITE = 1
+BLACK = 0
+
+CLOCK_FONT_FILE = '/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf'
+DATE_FONT_FILE  = '/usr/share/fonts/truetype/freefont/FreeMono.ttf'
+
+def main(argv):
+
+    """main program - draw and display time and date"""
+
+    papirus = Papirus(rotation = int(argv[0]) if len(sys.argv) > 1 else 0)
 
 
-# Clear all text from the screen
-# This does a full update so is a little slower than just removing the text.
-    text.Clear()
-    sleep (1)
+    papirus.clear()
+
+    demo(papirus)
+
+
+def demo(papirus):
+    """simple partial update demo - draw a clock"""
+
+    # initially set all white background
+    image = Image.new('1', papirus.size, WHITE)
+
+    # prepare for drawing
+    draw = ImageDraw.Draw(image)
+    width, height = image.size
+
+    clock_font_size = int((width - 4)/(8*0.65))      # 8 chars HH:MM:SS
+    clock_font = ImageFont.truetype(CLOCK_FONT_FILE, clock_font_size)
+    date_font_size = int((width - 10)/(10*0.65))     # 10 chars YYYY-MM-DD
+    date_font = ImageFont.truetype(DATE_FONT_FILE, date_font_size)
+
+    # clear the display buffer
+    draw.rectangle((0, 0, width, height), fill=WHITE, outline=WHITE)
+    previous_second = 0
+    previous_day = 0
+
+    while True:
+        while True:
+
+        draw.rectangle((5, 10, width - 5, 10 + clock_font_size), fill=WHITE, outline=WHITE)
+
+        draw.text((5, 10), "Hello world" + 1, fill=BLACK, font=clock_font)
+
+        papirus.partial_update()
+
+
+# main
+if "__main__" == __name__:
+    if len(sys.argv) < 1:
+        sys.exit('usage: {p:s}'.format(p=sys.argv[0]))
+
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        sys.exit('interrupted')
+        pass
