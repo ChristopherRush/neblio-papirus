6bd7b72a3e9db1754bd0bbe3ddc274da4803f0c9
ChristopherRush <chris.uclan@gmail.com>
2018-04-09 09:49:43 +0100

    removed

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from papirus import PapirusTextPos
 
-rot = 0
+
 # Same as calling "PapirusTextPos(True [,rotation = rot])"
-text = PapirusTextPos([rotation = rot])
+
 
 # Write text to the screen at selected point, with an Id
 # "hello world" will appear on the screen at (10, 10), font size 20, straight away
@@ -11,3 +11,5 @@ text.AddText("hello world", 10, 10, Id="Start" )
 # Add another line of text, at the default location
 # "Another line" will appear on screen at (0, 0), font size 20, straight away
 text.AddText("Another line", Id="Top")
+
+text.WriteAll()
