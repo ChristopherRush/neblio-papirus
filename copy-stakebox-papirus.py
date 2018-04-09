ea477e7cf6c445c3665878117aea86a7a3bc52de
ChristopherRush <chris.uclan@gmail.com>
2018-04-09 19:15:58 +0100

    sdfsdf

--- copy-stakebox-papirus.py
+++ copy-stakebox-papirus.py
@@ -181,3 +181,14 @@ def draw_image(papirus):
 
 
     sleep(0.1)
+
+# main
+if "__main__" == __name__:
+    if len(sys.argv) < 1:
+        sys.exit('usage: {p:s}'.format(p=sys.argv[0]))
+
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        sys.exit('interrupted')
+        pass
